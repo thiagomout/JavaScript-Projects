1b5efdbf7ac7688393d7cedd433817712ef6e606
thiagomout <thiagomoutinho19@gmail.com>
2023-05-29 21:45:13 -0300

    Create dog_years.js

---
+++ dog_years.js
@@ -0,0 +1,20 @@
+const myAge = 21 ; //enter here your age
+
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2; // we already accounted first 2 years
+
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+ageInDogYears = earlyYears + laterYears;
+
+const myName = 'Thiago'
+
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${ageInDogYears} in dog years.`)
+
